b7bfa1303f9456dd47850303686276ac786b0291
= <=>
2022-09-21 17:10:38 +0200

    Created a Map view that shows the user's current location

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,13 +1,24 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -15,13 +26,35 @@ import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
+
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
 
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                val enabled = enableMyLocation()
+                if (enabled) {
+                    fusedLocationProviderClient
+                        .requestLocationUpdates(
+                            locationRequest,
+                            locationCallback,
+                            Looper.getMainLooper()
+                        )
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -36,15 +69,34 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         setDisplayHomeAsUpEnabled(true)
 
         //add the map setup implementation
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
 
-//        TODO: zoom to the user location after taking his permission
+        // Make Location request
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    val lng = location.longitude
+                    val lat = location.latitude
+                    val zoomLevel = 18f
+                    val userLarLng = LatLng(lat, lng)
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLarLng, zoomLevel))
+                    map.addMarker(MarkerOptions().position(userLarLng))
+                }
+
+            }
+        }
+
+
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
-
 
 //        TODO: call this function after the user confirms on the selected location
         onLocationSelected()
@@ -85,7 +137,41 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        Log.d(TAG, "onMapReady")
         map = googleMap
+
+        val enabled = enableMyLocation()
+        if (enabled) {
+            fusedLocationProviderClient
+                .requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+        }
+
+    }
+
+    private fun enableMyLocation(): Boolean {
+        return if (isPermissionGranted()) {
+            Log.d(TAG, "Permission Granted!")
+            map.setMyLocationEnabled(true)
+            true
+        } else {
+            Log.d(TAG, "Request Permission")
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            false
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat
+            .checkSelfPermission(requireActivity().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private var TAG = SelectLocationFragment::class.java.simpleName
     }
 
 
